82e83a829945c662d7ddd20d52b6b2dc9fe89b67
Rajmeetkaur <73395163+Rajmeetkaur@users.noreply.github.com>
2020-11-07 13:42:31 +0530

    Create Assignment 1.js

---
+++ Assignment 1.js
@@ -0,0 +1,159 @@
+# Solution 1
+var x= 0;
+var y=-1;
+var z= 4;
+if (x>y && x>z)
+{
+        if (y>z)
+        {
+            console.log(x + ", " + y + ", " +z);
+        }
+        else
+        {
+            console.log(x + ", " + z + ", " +y);
+        }
+}
+else if (y>x && y >z)
+{
+        if (x>z)
+        {
+             console.log(y + ", " + x + ", " +z);
+        }
+        else
+        {
+             console.log(y + ", " + z + ", " +x);
+        }
+}
+else if (z>x && z>y)
+{
+        if (x>y)
+        {
+            console.log(z + ", " + x + ", " +y);
+        }
+        else
+        {
+            console.log(z + ", " + y + ", " +x);
+        }
+}       
+
+# solution 2
+for (var x=0; x<=15; x++) {
+        if (x === 0) {
+                console.log(x +  " is even");
+        }
+        else if (x % 2 === 0) {
+                console.log(x + " is even");   
+        }
+        else {
+                console.log(x + " is odd");
+        }
+}
+
+# solution 3
+let y = 10;
+            let x = 10;
+
+            let str = "";
+
+            for(let i = 1; i < y; i++ ){
+                for(let j = 1; j < x; j++){
+                    if(i + j >= y){
+                        str = str.concat("*");
+                    }else{
+                        str = str.concat(" ")
+                    }
+                }
+                str = str.concat("\n")
+            }
+
+            console.log(str)
+
+# solution 4
+protect_email = function (user_email) {
+    var avg, splitted, part1, part2;
+    splitted = user_email.split("@");
+    part1 = splitted[0];
+    avg = part1.length / 2;
+    part1 = part1.substring(0, (part1.length - avg));
+    part2 = splitted[1];
+    return part1 + "...@" + part2;
+};
+
+console.log(protect_email("rajmeet_kaur@example.com"));
+
+# solution 5
+function capital_letter(str) 
+{
+    str = str.split(" ");
+
+    for (var i = 0, x = str.length; i < x; i++) {
+        str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+    }
+
+    return str.join(" ");
+}
+
+console.log(capital_letter("Write a JavaScript program to capitalize the first letter of each word of a given string."));
+
+# solution 6
+function truncate(str, no_words) {
+    return str.split(" ").splice(0,no_words).join(" ");
+}
+
+console.log(truncate('The quick brown fox jumps over the lazy dog', 4));
+
+# solution 7
+function find_longest_word(str)
+{
+  var array1 = str.match(/\w[a-z]{0,}/gi);
+  var result = array1[0];
+
+  for(var x = 1 ; x < array1.length ; x++)
+  {
+    if(result.length < array1[x].length)
+    {
+    result = array1[x];
+    } 
+  }
+  return result;
+}
+console.log(find_longest_word('Web Development Tutorial'));
+
+# solution 8
+let number = parseInt(prompt('Enter the number of terms: '));
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
+
+# solution 9
+function fToC(fahrenheit) 
+{
+  const fTemp = fahrenheit;
+  const fToCel = (fTemp - 32) * 5 / 9;
+  const message = `${fTemp}\xB0F is ${fToCel}\xB0C.`;
+    console.log(message);
+} 
+function cToF(celsius) 
+{
+  const cTemp = celsius;
+  const cToFahr = cTemp * 9 / 5 + 32;
+  const message = `${cTemp}\xB0C is ${cToFahr} \xB0F.`;
+    console.log(message);
+}
+fToC(45);
+fToC(60)
+
+# solution 10
+swapcase = function swapcase(str) {
+        return str.replace(/([a-z]+)|([A-Z]+)/g, function(match, chr) {
+            return chr ? match.toUpperCase() : match.toLowerCase();
+        });
+    }
+console.log(swapcase('AaBbc'));
